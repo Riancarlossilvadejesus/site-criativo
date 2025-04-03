caa0f2bc1b4ab572182525b201737ee358d6fce2
Riancarlossilvadejesus <148889269+Riancarlossilvadejesus@users.noreply.github.com>
2025-04-03 12:33:24 +0000

    rian

---
+++ site-acessivel-projeto-inicial (1)/site-acessivel-projeto-inicial/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',function()){
+    const aumentarFonte=document.getElementById('aumentar-fonte');
+
+   let tamanhoAtualFonte=1;
+
+   let tamanhoAtualFonteBotao.addEventListener('click',function()){
+        
